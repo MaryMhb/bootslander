7a1d6dc42c309ac0f8a52aa5c1ce4ef90ede00cf
MaryMhb <maryam.mohebbi1996@gmail.com>
2024-08-19 00:49:03 +0430

    migration, new table , admin modification
    making database
    building new superuser
    new table for users
    registering user table in admin

--- landing/admin.py
+++ landing/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from landing.models import User
+class UserAdmin (admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    empty_value_display = '-empty-'
+    list_display = ('username' ,'email','created_date')
+    list_filter = ('email',)
+    search_fields = ['username',]
+admin.site.register(User,UserAdmin)
--- landing/models.py
+++ landing/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=255)
+    age = models.IntegerField(default=0)
+    email = models.EmailField (null=True)
+    password = models.IntegerField()
+    name = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+    created_date = models.DateTimeField(auto_now=True)
+    class Meta:
+        ordering = ['created_date']
+    def __str__(self):
+        return self.username
